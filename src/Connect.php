eee980840c01ec9cc7cb87919dc1147a85733ee6
duynv <duynv1499@gmail.com>
2023-01-31 11:26:47 +0700

    Bpm/list

--- src/Connect.php
+++ src/Connect.php
@@ -34,4 +34,27 @@ class Connect {
             return $e->getMessage();
         }
     }
+    public function getBpmList($data) {
+        $Common = new Common();
+        $jwt = getRequirement::$_jwt;
+        try{
+            $header = [
+                'jwt' => 'Bearer '.$jwt,
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+            ];
+            $rs = $Common->requestAPI('GET', API_URL.'/payment/api/v5/bpm/list', $header, [], $data);
+            $responseMassage = $rs->body->message;
+            $dataRespone = [
+                'responseCode' => 01,
+                'responseMessage' => ($responseMassage) ? $responseMassage: ''
+            ];
+            if ($rs->body->code == 0) {
+                $dataRespone['data'] = $rs->body->data;
+            }
+            return $dataRespone;
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
 }
